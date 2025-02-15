420562f2d186e9c93f5158c6962b24ceb543f62e
Subhash Yogi <subhashyogi999@gmail.com>
2025-02-15 13:54:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const scorePlayer_1 = document.querySelector("#score--1");
-const currentScorePlayer_1 = document.getElementById("current--1").textContent;
-const scorePlayer_2 = document.querySelector('#score--2');
-const currentScorePlayer_2 = document.getElementById('current--2');
+const scorePlayer1 = document.querySelector("#score--1");
+// const currentScorePlayer1 = document.getElementById("current--1").textContent;
+const scorePlayer2 = document.querySelector("#score--2");
+// const currentScorePlayer2 = document.getElementById("current--2");
 const newGame = document.querySelector(".newGame");
 const rollDice = document.querySelector(".rollDice");
 const hold = document.querySelector(".hold");
@@ -11,13 +11,12 @@ const player_1 = document.querySelector(".player--1");
 const player_2 = document.querySelector(".player--2");
 const name_1 = document.querySelector("#name--1");
 const name_2 = document.querySelector("#name--2");
-const label = document.querySelector('.alter');
-const alterChance = document.querySelector('#alter-Chance');
-const send = document.querySelector('#start');
-const inpname = document.querySelector('.inpName');
-const inpbox = document.querySelector('.inpBox');
-const img = document.querySelector('.img');
-
+const label = document.querySelector(".alter");
+const alterChance = document.querySelector("#alter-Chance");
+const send = document.querySelector("#start");
+const inpname = document.querySelector(".inpName");
+const inpbox = document.querySelector(".inpBox");
+const img = document.querySelector(".img");
 
 let activePlayer, currentScore, scorePlayer, playing;
 
@@ -26,12 +25,12 @@ const reset = function () {
   scorePlayer = [0, 0];
   activePlayer = 1;
   playing = true;
-  alterChance.checked = false;
+  // alterChance.checked = false;
 
-  scorePlayer_1.textContent = 0;
-  scorePlayer_2.textContent = 0;
-  name_1.textContent = 'player 1'
-  name_2.textContent = 'player 2'
+  scorePlayer1.textContent = 0;
+  scorePlayer2.textContent = 0;
+  name_1.textContent = "player 1";
+  name_2.textContent = "player 2";
   document.getElementById("current--1").textContent = 0;
   document.getElementById("current--2").textContent = 0;
 
@@ -39,10 +38,7 @@ const reset = function () {
   player_2.classList.remove("winner");
   player_1.classList.add("player-active");
   player_2.classList.remove("player-active");
- 
-
-  
-}
+};
 reset();
 
 const switchPlayer = function () {
@@ -56,12 +52,10 @@ const switchPlayer = function () {
 // roll to dice
 rollDice.addEventListener("click", () => {
   if (playing) {
-    
     // generate dice number
     let diceNumber = Math.trunc(Math.random() * 6) + 1;
-
+    // console.log(diceNumber);
     // Display dice
-
     img.src = `dice-${diceNumber}.png`;
     // check dice number is 1
     if (diceNumber !== 1) {
@@ -86,7 +80,7 @@ hold.addEventListener("click", () => {
       scorePlayer[activePlayer - 1];
 
     // winner display
-    if (scorePlayer[activePlayer - 1] >= 20) {
+    if (scorePlayer[activePlayer - 1] >= 50) {
       playing = false;
       // finish the game
       document.querySelector(`#name--${activePlayer}`).textContent =
@@ -105,9 +99,9 @@ hold.addEventListener("click", () => {
 });
 
 //for new game
-newGame.addEventListener('click', reset);
+newGame.addEventListener("click", reset);
 
-const hideShow = function() {
+const hideShow = function () {
   if (!inpbox.classList.contains("hide") || inpbox.classList.contains("show")) {
     inpbox.classList.add("hide");
     inpbox.classList.remove("show");
@@ -117,205 +111,176 @@ const hideShow = function() {
   }
   player_1_name.value = "";
   player_2_name.value = "";
-}
+};
 
-inpname.addEventListener('click',hideShow)
+inpname.addEventListener("click", hideShow);
 
-send.addEventListener(
-  'click',
-  (e) => {
-    e.preventDefault();
-    let player_1_name = document.querySelector("#player_1_name").value;
-    let player_2_name = document.querySelector("#player_2_name").value;
-    if (player_1_name === '' && player_2_name === '') {
-        document.querySelector("#name--1").textContent = 'player 1';
-        document.querySelector("#name--2").textContent = 'player 2';
-         hideShow();
-    }
-    else {
-
-      document.querySelector("#name--1").textContent = player_1_name;
-      document.querySelector("#name--2").textContent = player_2_name;
-      hideShow();
-    }
-    
+send.addEventListener("click", (e) => {
+  e.preventDefault();
+  let player_1_name = document.querySelector("#player_1_name").value;
+  let player_2_name = document.querySelector("#player_2_name").value;
+  if (player_1_name === "" && player_2_name === "") {
+    document.querySelector("#name--1").textContent = "player 1";
+    document.querySelector("#name--2").textContent = "player 2";
+    hideShow();
+  } else {
+    document.querySelector("#name--1").textContent = player_1_name;
+    document.querySelector("#name--2").textContent = player_2_name;
+    hideShow();
   }
-)
-<<<<<<< HEAD
-=======
+});
+// <<<<<<< HEAD
+// =======
 // console.log(player_1_name);
 // console.log(player_2_name);
 
-
 // New Game button
-newGame.addEventListener(
-    'click',
-    (e) => {
-        document.querySelector("#score--1").textContent = 0;
-        document.querySelector("#current--1").textContent = 0;
-        document.querySelector("#score--2").textContent = 0;
-        document.querySelector("#current--2").textContent = 0;
-        document.querySelector("#name--1").textContent = 'Player 1';
-        document.querySelector("#name--2").textContent = 'Player 2';
-        toActivePlayer_1();
-        // active();
-        scorePlayer_1 = 0;
-        scorePlayer_2 = 0;
-        alterChance.checked = false;
-        rollDice.disabled = false;
-        hold.disabled = false;
-        alterChance.disabled = true;
-    }
-)
-const toActivePlayer_1 = function() {
-    player_2.classList.remove("player-active");
-    player_1.classList.add("player-active");
-}
-const toActivePlayer_2 = function() {
-    player_1.classList.remove("player-active");
-    player_2.classList.add("player-active");
-}
+newGame.addEventListener("click", (e) => {
+  document.querySelector("#score--1").textContent = 0;
+  document.querySelector("#current--1").textContent = 0;
+  document.querySelector("#score--2").textContent = 0;
+  document.querySelector("#current--2").textContent = 0;
+  document.querySelector("#name--1").textContent = "Player 1";
+  document.querySelector("#name--2").textContent = "Player 2";
+  toActivePlayer_1();
+  // active();
+  scorePlayer_1 = 0;
+  scorePlayer_2 = 0;
+  // alterChance.checked = false;
+  rollDice.disabled = false;
+  hold.disabled = false;
+  alterChance.disabled = true;
+});
+const toActivePlayer_1 = function () {
+  player_2.classList.remove("player-active");
+  player_1.classList.add("player-active");
+};
+const toActivePlayer_2 = function () {
+  player_1.classList.remove("player-active");
+  player_2.classList.add("player-active");
+};
 //palyer active functon
 const active = function () {
-    if (
-      player_1.classList.contains("player-active") &&
-      !player_2.classList.contains("player-active")
-    ){
-        //  toActivePlayer_2();
-        player_1.classList.remove("player-active");
-        player_2.classList.add("player-active");
-    }
-    else if (
-      !player_1.classList.contains("player-active") &&
-      player_2.classList.contains("player-active")
-    ) {
-      //  toActivePlayer_1();
-      player_2.classList.remove("player-active");
-      player_1.classList.add("player-active");
-    }
-     
-}
+  if (
+    player_1.classList.contains("player-active") &&
+    !player_2.classList.contains("player-active")
+  ) {
+    //  toActivePlayer_2();
+    player_1.classList.remove("player-active");
+    player_2.classList.add("player-active");
+  } else if (
+    !player_1.classList.contains("player-active") &&
+    player_2.classList.contains("player-active")
+  ) {
+    //  toActivePlayer_1();
+    player_2.classList.remove("player-active");
+    player_1.classList.add("player-active");
+  }
+};
 let scorePlayer_1 = 0;
 //current score of player 1
-const currentScorePlayer_1 = function() {
-    const diceNumber = Math.trunc(Math.random() * 6) + 1;
-    // console.log(diceNumber);
-    // console.log("player 1");
-    // console.log(currentScorePlayer_1);
-    if (diceNumber === 1) {
-        document.querySelector("#score--1").textContent = 0;
-        document.querySelector("#current--1").textContent = 0;
-        active();
-        scorePlayer_1 = 0;
+const currentScorePlayer_1 = function () {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  // console.log(diceNumber);
+  // console.log("player 1");
+  // console.log(currentScorePlayer_1);
+  if (diceNumber === 1) {
+    document.querySelector("#score--1").textContent = 0;
+    document.querySelector("#current--1").textContent = 0;
+    active();
+    scorePlayer_1 = 0;
+  } else {
+    scorePlayer_1 += diceNumber;
+    // console.log('player 1 score' + scorePlayer_1);
+    let c = (document.querySelector("#current--1").textContent = scorePlayer_1);
+    if (c >= Max) {
+      document.querySelector("#name--1").textContent = "YOU WON!!!";
+      document.querySelector("#score--1").textContent = scorePlayer_1;
+      document.querySelector("#current--1").textContent = 0;
+      // scorePlayer_1 = 0;
+      toActivePlayer_1();
+      rollDice.disabled = true;
+      hold.disabled = true;
     }
-    else {
-
-      scorePlayer_1 += diceNumber;
-      // console.log('player 1 score' + scorePlayer_1);
-     let c = document.querySelector("#current--1").textContent = scorePlayer_1;
-      if (c >= Max) {
-        document.querySelector("#name--1").textContent = "YOU WON!!!";
-        document.querySelector("#score--1").textContent = scorePlayer_1;
-        document.querySelector("#current--1").textContent = 0;
-        // scorePlayer_1 = 0;
-        toActivePlayer_1();
-        rollDice.disabled = true;
-        hold.disabled = true;
-       
-      }
-      return scorePlayer_1;
-    }
+    return scorePlayer_1;
   }
-
+};
 
 //current score of player 2
 
 let scorePlayer_2 = 0;
-const currentScorePlayer_2 = function() {
-    
-    const diceNumber = Math.trunc(Math.random() * 6) + 1;
-    // console.log(diceNumber);
-    // console.log('player 2');
-    // console.log(currentScorePlayer_1);
-    if (diceNumber === 1) {
-      document.querySelector("#score--2").textContent = 0;
-      document.querySelector("#current--2").textContent = 0;
-      active();
-      scorePlayer_2 = 0;
-    }
-    else {
-
-      scorePlayer_2 += diceNumber;
-      // console.log("player 2 score " + scorePlayer_2);
-     let c = document.querySelector("#current--2").textContent = scorePlayer_2;
-      if (c >= Max) {
-        document.querySelector("#name--2").textContent = "YOU WON!!!";
-        document.querySelector("#score--2").textContent = scorePlayer_2;
-        document.querySelector("#current--2").textContent = 0;
-        // scorePlayer_1 = 0;
-        toActivePlayer_2();
-        rollDice.disabled = true;
-        hold.disabled = true;
-        alterChance.disabled = true;
-      }
-      return scorePlayer_2;
-    }
-}
-
-
-//current data function for both players
-const Max = 100;
-const currentScoreBtnPlayer_1 = function () {
-   
-    if (scorePlayer_1 >= Max) {
-      document.querySelector("#name--1").textContent = 'YOU WON!!!';
-      document.querySelector("#score--1").textContent = scorePlayer_1;
-      document.querySelector("#current--1").textContent = 0;
-      // scorePlayer_1 = 0;
-      rollDice.disabled = true;
-      hold.disabled = true;
-    }
-    else {
-      // console.log('player 1 score ' + scorePlayer_1);
-      document.querySelector("#current--1").textContent = 0;
-      // console.log(score);
-      document.querySelector("#score--1").textContent = scorePlayer_1;
-      active();
-    }
-    // toActivePlayer_2();
-};
-const currentScoreBtnPlayer_2 = function () {
-    
-    if (scorePlayer_2 >= Max){
-      document.querySelector("#name--2").textContent = 'YOU WON!!!';
-      document.querySelector("#current--2").textContent = 0;
-      // console.log(score);
+const currentScorePlayer_2 = function () {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  // console.log(diceNumber);
+  // console.log('player 2');
+  // console.log(currentScorePlayer_1);
+  if (diceNumber === 1) {
+    document.querySelector("#score--2").textContent = 0;
+    document.querySelector("#current--2").textContent = 0;
+    active();
+    scorePlayer_2 = 0;
+  } else {
+    scorePlayer_2 += diceNumber;
+    // console.log("player 2 score " + scorePlayer_2);
+    let c = (document.querySelector("#current--2").textContent = scorePlayer_2);
+    if (c >= Max) {
+      document.querySelector("#name--2").textContent = "YOU WON!!!";
       document.querySelector("#score--2").textContent = scorePlayer_2;
+      document.querySelector("#current--2").textContent = 0;
+      // scorePlayer_1 = 0;
+      toActivePlayer_2();
       rollDice.disabled = true;
       hold.disabled = true;
       alterChance.disabled = true;
     }
-    else {
-      document.querySelector("#current--2").textContent = 0;
-      // console.log(score);
-      document.querySelector("#score--2").textContent = scorePlayer_2;
-    }
-    // active();
-    // toActivePlayer_1();
+    return scorePlayer_2;
+  }
+};
 
+//current data function for both players
+const Max = 100;
+const currentScoreBtnPlayer_1 = function () {
+  if (scorePlayer_1 >= Max) {
+    document.querySelector("#name--1").textContent = "YOU WON!!!";
+    document.querySelector("#score--1").textContent = scorePlayer_1;
+    document.querySelector("#current--1").textContent = 0;
+    // scorePlayer_1 = 0;
+    rollDice.disabled = true;
+    hold.disabled = true;
+  } else {
+    // console.log('player 1 score ' + scorePlayer_1);
+    document.querySelector("#current--1").textContent = 0;
+    // console.log(score);
+    document.querySelector("#score--1").textContent = scorePlayer_1;
+    active();
+  }
+  // toActivePlayer_2();
+};
+const currentScoreBtnPlayer_2 = function () {
+  if (scorePlayer_2 >= Max) {
+    document.querySelector("#name--2").textContent = "YOU WON!!!";
+    document.querySelector("#current--2").textContent = 0;
+    // console.log(score);
+    document.querySelector("#score--2").textContent = scorePlayer_2;
+    rollDice.disabled = true;
+    hold.disabled = true;
+    alterChance.disabled = true;
+  } else {
+    document.querySelector("#current--2").textContent = 0;
+    // console.log(score);
+    document.querySelector("#score--2").textContent = scorePlayer_2;
+  }
+  // active();
+  // toActivePlayer_1();
 };
 
 // hold button for both players
 const holdBtn = function (player) {
-    
-    //one by one chance of playing 
-    // active();
-      hold.addEventListener("click", player);
-      active();
-}
->>>>>>> f703a04c75549fe95711421aefa964beaba51a36
-
-
+  //one by one chance of playing
+  // active();
+  hold.addEventListener("click", player);
+  active();
+};
 
 // // console.log(player_1_name);
 // // console.log(player_2_name);
